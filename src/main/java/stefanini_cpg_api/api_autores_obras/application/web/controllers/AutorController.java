b8d55ffd67fcbd3e3679495f20d09efdbd680906
tempjunior <junior_sadaanzinhoa@hotmail.com>
2025-09-01 22:02:27 -0300

    Implementando endPoints de consulta e delete

--- src/main/java/stefanini_cpg_api/api_autores_obras/application/web/controllers/AutorController.java
+++ src/main/java/stefanini_cpg_api/api_autores_obras/application/web/controllers/AutorController.java
@@ -1,6 +1,7 @@
 package stefanini_cpg_api.api_autores_obras.application.web.controllers;
 
 import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import stefanini_cpg_api.api_autores_obras.application.web.dto.request.AutorRequestDTO;
+import stefanini_cpg_api.api_autores_obras.application.web.dto.response.ArtworkResponseDTO;
 import stefanini_cpg_api.api_autores_obras.application.web.dto.response.AutorArtworkResponseDTO;
 import stefanini_cpg_api.api_autores_obras.application.web.dto.response.AutorResponseDTO;
 import stefanini_cpg_api.api_autores_obras.domain.services.impl.AutorServiceImpl;
@@ -34,10 +36,23 @@ public class AutorController {
         return ResponseEntity.ok(page);
     }
 
-    @GetMapping
-    public ResponseEntity<Page<AutorArtworkResponseDTO>> getAllArtworksOfAutor(){
+    @GetMapping("/artworks/by-id/{id}")
+    public ResponseEntity<Page<ArtworkResponseDTO>> getAllArtworksOfAutor(@PageableDefault(size = 10, sort = {"name"}) Pageable pagination, @PathVariable Long id){
+        var page = this.autorService.getAllArtworks(id, pagination);
 
+        return ResponseEntity.ok(page);
     }
 
+    @GetMapping("artworks/by-name/{autorName}")
+    public ResponseEntity<Page<AutorArtworkResponseDTO>>getAllArtworksByAutorName(@PageableDefault(size = 10, sort = {"name"}) Pageable pagination, @PathVariable String autorName){
+        var page = this.autorService.getAllArtworksByAutorName(pagination, autorName);
+        return ResponseEntity.ok(page);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(Long id){
+        this.autorService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
