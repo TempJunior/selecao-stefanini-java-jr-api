999ccdbffddc933a6e116deb6baf26ae2c253b42
tempjunior <junior_sadaanzinhoa@hotmail.com>
2025-09-01 22:25:21 -0300

    Ajustando validação de regexp

--- src/main/java/stefanini_cpg_api/api_autores_obras/application/web/dto/request/AutorRequestDTO.java
+++ src/main/java/stefanini_cpg_api/api_autores_obras/application/web/dto/request/AutorRequestDTO.java
@@ -23,7 +23,7 @@ public record AutorRequestDTO(
 
         Gender gender,
 
-        @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$\n",
+        @Pattern(regexp = "^([0-9a-zA-Z]+([_.-]?[0-9a-zA-Z]+)*@[0-9a-zA-Z]+[0-9,a-z,A-Z,.,-]*(.){1}[a-zA-Z]{2,4})+$",
                 message = "Email format invalid")
         String email,
 
