91d997233bfe194d8c934df6d0edac29f31f0e35
TempJunior <junior_sadaanzinhoa@hotmail.com>
2025-09-08 09:57:28 -0300

    Ajuste Repository

--- src/main/java/stefanini_cpg_api/api_autores_obras/domain/services/ArtworkService.java
+++ src/main/java/stefanini_cpg_api/api_autores_obras/domain/services/ArtworkService.java
@@ -3,7 +3,6 @@ package stefanini_cpg_api.api_autores_obras.domain.services;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import stefanini_cpg_api.api_autores_obras.application.web.dto.request.ArtworkRequestDTO;
-import stefanini_cpg_api.api_autores_obras.application.web.dto.response.ArtworkAutorResponseDTO;
 import stefanini_cpg_api.api_autores_obras.application.web.dto.response.ArtworkResponseDTO;
 import stefanini_cpg_api.api_autores_obras.application.web.dto.response.AutorResponseDTO;
 import stefanini_cpg_api.api_autores_obras.domain.entities.Artwork;
